cb07bd744b6fb974a22255cc21247ebc8220a8f9
Juan David <juandavid918@gmail.com>
2020-06-04 11:29:25 -0500

    Iniciando repaso JS

---
+++ repasojs/js/01-variables.js
@@ -0,0 +1,14 @@
+// Crear variables
+
+// Variables con var
+// var aprendiendo = "JavaScript";
+
+// Variables con const
+// const aprendiendo = "JavaScript";
+
+// Variables con let
+let aprendiendo = "JavaScript";
+
+aprendiendo = true;
+
+console.log(aprendiendo);
---
+++ repasojs/js/app.js
@@ -0,0 +1,9 @@
+// Scope
+
+var musica = "Rock";
+
+if (musica) {
+    var musica = "Grunge";
+    console.log("dentro del if: ", musica);
+}
+console.log("fuera del if: ", musica);
