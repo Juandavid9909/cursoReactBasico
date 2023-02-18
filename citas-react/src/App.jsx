de5b824203b9222d288b7608dc8393475af43b92
Juan David <juandavid918@gmail.com>
2023-02-18 17:23:03 -0500

    useState chequeado

--- citas-react/src/App.jsx
+++ citas-react/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 
-function App() {
+const App = () => {
 
   return (
     <div className="container mx-auto mt-20">
