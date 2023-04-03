9353f2c4be607c6bb8cd0f51207741fe8dd21f47
Juan David <juandavid918@gmail.com>
2023-04-03 15:51:59 -0500

    Proyecto control gastos finalizado

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 
 import Header from "./components/Header";
+import Filtros from './components/Filtros';
 import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
 
@@ -10,15 +11,23 @@ import IconoNuevoGasto from "./img/nuevo-gasto.svg";
 
 function App() {
 
-  const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem("gastos") ? JSON.parse(localStorage.getItem("gastos")) : []
+  );
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem("presupuesto")) ?? 0
+  );
+
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
 
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
 
   const [gastoEditar, setGastoEditar] = useState({});
+
+  const [filtro, setFiltro] = useState("");
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
 
   useEffect(() => {
     if(Object.keys(gastoEditar).length > 0) {
@@ -29,6 +38,30 @@ function App() {
       }, 500);
     }
   }, [gastoEditar]);
+
+  useEffect(() => {
+    localStorage.setItem("presupuesto", presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    localStorage.setItem("gastos", JSON.stringify(gastos) ?? []);
+  }, [gastos]);
+
+  useEffect(() => {
+    if(filtro) {
+      const gastosFiltradosArr = gastos.filter(gasto => gasto.categoria === filtro);
+
+      setGastosFiltrados(gastosFiltradosArr);
+    }
+  }, [filtro]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem("presupuesto")) ?? 0;
+
+    if(presupuestoLS > 0) {
+      setIsValidPresupuesto(true);
+    }
+  }, []);
 
   const handleNuevoGasto = () => {
     setModal(true);
@@ -71,6 +104,7 @@ function App() {
     <div className={ modal ? "fijar" : ""}>
       <Header
         gastos={ gastos }
+        setGastos={ setGastos }
         presupuesto={ presupuesto }
         setPresupuesto={ setPresupuesto }
         isValidPresupuesto={ isValidPresupuesto }
@@ -81,10 +115,17 @@ function App() {
       { isValidPresupuesto && (
         <>
           <main>
+            <Filtros
+              filtro={ filtro }
+              setFiltro={ setFiltro }
+            />
+
             <ListadoGastos
               gastos={ gastos }
               setGastoEditar={ setGastoEditar }
               eliminarGasto={ eliminarGasto }
+              filtro={ filtro }
+              gastosFiltrados={ gastosFiltrados }
             />
           </main>
 
@@ -104,6 +145,7 @@ function App() {
                   setAnimarModal={ setAnimarModal }
                   guardarGasto={ guardarGasto }
                   gastoEditar={ gastoEditar }
+                  setGastoEditar={ setGastoEditar }
                 /> }
     </div>
   )
--- control-gastos/src/components/ListadoGastos.jsx
+++ control-gastos/src/components/ListadoGastos.jsx
@@ -1,18 +1,35 @@
 import Gasto from "./Gasto";
 
-const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados }) => {
     return (
         <div className="listado-gastos contenedor">
-            <h2>{ gastos.length ? "Gastos" : "No hay gastos aún" }</h2>
+            { filtro ? (
+                <>
+                    <h2>{ gastosFiltrados.length ? "Gastos" : "No hay gastos en esta categoría" }</h2>
 
-            { gastos.map(gasto => (
-                <Gasto
-                    key={ gasto.id }
-                    gasto={ gasto }
-                    setGastoEditar={ setGastoEditar }
-                    eliminarGasto={ eliminarGasto }
-                />
-            )) }
+                    { gastosFiltrados.map(gasto => (
+                        <Gasto
+                            key={ gasto.id }
+                            gasto={ gasto }
+                            setGastoEditar={ setGastoEditar }
+                            eliminarGasto={ eliminarGasto }
+                        />
+                    )) }
+                </>
+            ) : (
+                <>
+                    <h2>{ gastos.length ? "Gastos" : "No hay gastos aún" }</h2>
+
+                    { gastos.map(gasto => (
+                        <Gasto
+                            key={ gasto.id }
+                            gasto={ gasto }
+                            setGastoEditar={ setGastoEditar }
+                            eliminarGasto={ eliminarGasto }
+                        />
+                    )) }
+                </>
+            ) }
         </div>
     );
 }
