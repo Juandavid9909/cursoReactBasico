7414f1bb38ad8723402c91e9cebe951beeb46f83
Juan David <juandavid918@gmail.com>
2023-05-20 15:55:32 -0500

    Funcionalidad principal finalizada en guitarras y blogs

--- guitarla-remix/app/routes/tienda.jsx
+++ guitarla-remix/app/routes/tienda.jsx
@@ -1,15 +1,50 @@
+import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "../models/guitarras.server";
+import Guitarra from "~/components/guitarra";
+import styles from "../styles/guitarras.css";
+
+export function meta() {
+    return [
+        {
+            title: "GuitarLA - Tienda de Guitarras",
+            description: "GuitarLA - Nuestra colección de guitarras"
+        }
+    ];
+}
+
+export function links() {
+    return [
+        {
+            rel: "stylesheet",
+            href: styles
+        }
+    ];
+}
 
 export const loader = async() => {
     const guitarras = await getGuitarras();
-    console.log(guitarras);
 
-    return guitarras;
+    return guitarras.data;
 }
 
 const Tienda = () => {
+    const guitarras = useLoaderData();
+
     return (
-        <div>Tienda</div>
+        <main className="contenedor">
+            <h2 className="heading">Nuestra Colección</h2>
+
+            { guitarras?.length && (
+                <div className="guitarras-grid">
+                    { guitarras.map(guitarra => (
+                        <Guitarra
+                            key={ guitarra?.id }
+                            guitarra={ guitarra?.attributes }
+                        />
+                    )) }
+                </div>
+            ) }
+        </main>
     );
 }
 
