8b20022f78b24352d6cd26758980dc781ce916aa
Juan David <juandavid918@gmail.com>
2023-09-23 12:44:41 -0500

    App noticias finalizada

---
+++ noticiasapi/src/App.jsx
@@ -0,0 +1,43 @@
+import { Container , Grid, Typography} from '@mui/material';
+
+import { NoticiasProvider } from './context/NoticiasProvider';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
+
+function App() {
+  return (
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography
+            align="center"
+            marginY={ 5 }
+            component="h1"
+            variant="h3"
+          >
+            Buscador de noticias
+          </Typography>
+        </header>
+
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid
+            item
+            xs={ 12 }
+            md={ 6 }
+          >
+            <Formulario />
+          </Grid>
+        </Grid>
+
+        <ListadoNoticias />
+      </Container>
+    </NoticiasProvider>
+  );
+}
+
+export default App
