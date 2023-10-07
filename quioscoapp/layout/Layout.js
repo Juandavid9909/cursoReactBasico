257ac826a6b294906cd3e8900ddb55b4e9f4084c
Juan David <juandavid918@gmail.com>
2023-10-07 14:23:07 -0500

    Consumiendo API para traer categorías y productos

--- quioscoapp/layout/Layout.js
+++ quioscoapp/layout/Layout.js
@@ -1,8 +1,29 @@
+import { ToastContainer } from "react-toastify";
 import Head from "next/head";
+import Modal from "react-modal";
 
+import ModalProducto from "@/components/ModalProducto";
 import Sidebar from "@/components/Sidebar";
+import useQuiosco from "@/hooks/useQuiosco";
+
+import "react-toastify/dist/ReactToastify.css";
+
+const customStyles = {
+    content: {
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)"
+    }
+};
+
+Modal.setAppElement("#__next");
 
 const Layout = ({ children, pagina }) => {
+    const { modal } = useQuiosco();
+
     return (
         <>
             <Head>
@@ -16,9 +37,22 @@ const Layout = ({ children, pagina }) => {
                 </aside>
 
                 <main className="md:w-8/12 xl:w-3/4 2xl:w-4/5 h-screen overflow-y-scroll">
-                    { children }
+                    <div className="p-10">
+                        { children }
+                    </div>
                 </main>
             </div>
+
+            { modal && (
+                <Modal
+                    isOpen={ modal }
+                    style={ customStyles }
+                >
+                    <ModalProducto />
+                </Modal>
+            ) }
+
+            <ToastContainer />
         </>
     );
 }
