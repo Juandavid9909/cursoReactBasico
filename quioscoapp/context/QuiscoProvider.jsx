3bda5c99af6ba816de013a5544d0faf77bdcff33
Juan David <juandavid918@gmail.com>
2023-10-15 12:17:51 -0500

    Quisco App finalizada

--- quioscoapp/context/QuiscoProvider.jsx
+++ quioscoapp/context/QuiscoProvider.jsx
@@ -82,7 +82,23 @@ const QuioscoProvider = ({ children }) => {
     const colocarOrden = async (e) => {
         e.preventDefault();
 
+        try {
+            await axios.post("/api/ordenes", { pedido, nombre, total, fecha: Date.now().toString() });
 
+            // Resetear la app
+            setCategoriaActual(categorias[0]);
+            setPedido([]);
+            setNombre("");
+            setTotal(0);
+
+            toast.success("Pedido Realizado Correctamente");
+
+            setTimeout(() => {
+                router.push("/");
+            }, 3000);
+        } catch (error) {
+            console.log(error);
+        }
     }
     
     return (
