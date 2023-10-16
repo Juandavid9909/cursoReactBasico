1427181f66dc538758c6cc8fc3d4793535ebd9a0
Juan David <juandavid918@gmail.com>
2023-10-16 13:30:51 -0500

    Creando servidor con Node

---
+++ Uptask_MERN/backend/routes/usuarioRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+import { registrar } from "../controllers/usuarioController.js";
+
+const router = express.Router();
+
+// Autenticación, Registro y Confirmación de Usuarios
+router.post("/", registrar);
+
+export default router;
